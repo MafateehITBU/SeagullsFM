702ef0cab5ae4731847d09f0f2106f5408245bfc
mohammed-f-zain <mohammed.f.zain@gmail.com>
2026-03-03 12:46:35 +0300

    test

--- moodfm-web/src/axiosConfig.js
+++ moodfm-web/src/axiosConfig.js
@@ -2,7 +2,11 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5001/api",
+  // Prefer explicit web API envs; fall back to local dev server
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    process.env.BACKEND_URL ||
+    "http://localhost:5001/api",
   headers: {
     "Content-Type": "application/json",
   },
